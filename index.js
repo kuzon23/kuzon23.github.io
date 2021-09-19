0f7378b9753e930269eef131f889b0eebfd7d76e
Matthew <46788940+kuzon23@users.noreply.github.com>
2021-09-20 00:51:14 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-    var clicked = 0;
     window.onmousemove = function (e) {
         var x = e.clientX;
         var midpoint = (window.screen.width/2);
@@ -14,29 +13,8 @@
         }
     }
 
-    function updateHTML(elmId, value) {
-        var elem = document.getElementById(elmId);
-        if(typeof elem !== 'undefined' && elem !== null) {
-            elem.innerHTML = value;
-          }
-    }
-
     function poop() {
         var links = ['https://www.youtube.com/watch?v=couz1CrlTdQ','https://www.youtube.com/watch?v=lP0GNfWSW30','https://www.youtube.com/watch?v=2VjpwYM05eo','https://www.youtube.com/watch?v=zfueGqaXp1g', 'https://www.youtube.com/watch?v=SCwcJsBYL3o', 'https://www.youtube.com/watch?v=APJZeNY6dKo', 'https://www.youtube.com/watch?v=IaLGdRx7prA', 'https://www.youtube.com/watch?v=QUqCWkPlLLc'];
         var capybara = links[Math.floor(Math.random()*links.length)];
         window.open(capybara);
     }
-
-    var title = document.getElementById('secret');
-    function secret() {
-        if (clicked==0) {
-            title.style.letterSpacing = '32px';
-            var clicked = 1;
-        }
-        else if (clicked==1) {
-            title.style.letterSpacing = 'auto';
-            var clicked = 0;
-        }
-        else { console.log('error') }
-    }
-    document.getElementById('secret').onclick = secret();
