7da2c8abeb871371f65400a3e9bcc41e44926595
Matthew <46788940+kuzon23@users.noreply.github.com>
2021-09-19 22:16:38 +1000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@
         else {
             document.getElementById('capybara').style.transform = 'rotateZ(0deg)';
         }
-        document.getElementById('projects').style.backgroundPosition = x.toString() + 'px';
     }
     function poop() {
         var links = ['https://www.youtube.com/watch?v=couz1CrlTdQ','https://www.youtube.com/watch?v=lP0GNfWSW30','https://www.youtube.com/watch?v=2VjpwYM05eo','https://www.youtube.com/watch?v=zfueGqaXp1g', 'https://www.youtube.com/watch?v=SCwcJsBYL3o', 'https://www.youtube.com/watch?v=APJZeNY6dKo', 'https://www.youtube.com/watch?v=IaLGdRx7prA', 'https://www.youtube.com/watch?v=QUqCWkPlLLc'];
