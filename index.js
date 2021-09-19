88f1f8237c54ea4c2f35ed195081a3dcda1f5741
Matthew <46788940+kuzon23@users.noreply.github.com>
2021-09-19 21:57:28 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
         var x = e.clientX;
         var midpoint = (window.screen.width/2);
 
-        if (x > (midpoint+300)) {
+        if (x > (midpoint+250)) {
             document.getElementById('capybara').style.transform = 'rotateZ(4deg)';
         }
-        else if (x < (midpoint-300)) {
+        else if (x < (midpoint-250)) {
             document.getElementById('capybara').style.transform = 'rotateZ(-4deg)';
         }
         else {
             document.getElementById('capybara').style.transform = 'rotateZ(0deg)';
         }
+        document.getElementByID('projects').style.background-position = (x/100)
     }
     function poop() {
         var links = ['https://www.youtube.com/watch?v=couz1CrlTdQ','https://www.youtube.com/watch?v=lP0GNfWSW30','https://www.youtube.com/watch?v=2VjpwYM05eo','https://www.youtube.com/watch?v=zfueGqaXp1g', 'https://www.youtube.com/watch?v=SCwcJsBYL3o', 'https://www.youtube.com/watch?v=APJZeNY6dKo', 'https://www.youtube.com/watch?v=IaLGdRx7prA', 'https://www.youtube.com/watch?v=QUqCWkPlLLc'];
