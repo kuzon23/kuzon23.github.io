b6d5f91224d576bf16ee441498fffc8597d79a78
Matthew <46788940+kuzon23@users.noreply.github.com>
2021-09-19 22:07:46 +1000

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,10 @@
         else {
             document.getElementById('capybara').style.transform = 'rotateZ(0deg)';
         }
+        document.getElementById('projects').style.backgroundPosition = `${x/1000}px`
     }
     function poop() {
         var links = ['https://www.youtube.com/watch?v=couz1CrlTdQ','https://www.youtube.com/watch?v=lP0GNfWSW30','https://www.youtube.com/watch?v=2VjpwYM05eo','https://www.youtube.com/watch?v=zfueGqaXp1g', 'https://www.youtube.com/watch?v=SCwcJsBYL3o', 'https://www.youtube.com/watch?v=APJZeNY6dKo', 'https://www.youtube.com/watch?v=IaLGdRx7prA', 'https://www.youtube.com/watch?v=QUqCWkPlLLc'];
         var capybara = links[Math.floor(Math.random()*links.length)];
         window.open(capybara);
-    }
-    for (;;) {
-        document.getElementById('projects').style.backgroundPosition = `${x/100}px`;
     }
