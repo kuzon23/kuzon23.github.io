bf1299c89452d3f2fa238a2412344378c7b114e0
Matthew <46788940+kuzon23@users.noreply.github.com>
2021-09-19 22:04:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -18,5 +18,5 @@
         window.open(capybara);
     }
     for (;;) {
-        document.getElementByID('projects').style.backgroundPosition = `${x/100}%`;
+        document.getElementById('projects').style.backgroundPosition = `${x/100}%`;
     }
