4308eb06d362ee54a107c54d3dd97bd52ddd586f
Matthew <46788940+kuzon23@users.noreply.github.com>
2021-09-20 00:33:32 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+    var clicked = 'false';
     window.onmousemove = function (e) {
         var x = e.clientX;
         var midpoint = (window.screen.width/2);
@@ -12,8 +13,22 @@
             document.getElementById('capybara').style.transform = 'rotateZ(0deg)';
         }
     }
+
     function poop() {
         var links = ['https://www.youtube.com/watch?v=couz1CrlTdQ','https://www.youtube.com/watch?v=lP0GNfWSW30','https://www.youtube.com/watch?v=2VjpwYM05eo','https://www.youtube.com/watch?v=zfueGqaXp1g', 'https://www.youtube.com/watch?v=SCwcJsBYL3o', 'https://www.youtube.com/watch?v=APJZeNY6dKo', 'https://www.youtube.com/watch?v=IaLGdRx7prA', 'https://www.youtube.com/watch?v=QUqCWkPlLLc'];
         var capybara = links[Math.floor(Math.random()*links.length)];
         window.open(capybara);
+    }
+
+    function secret() {
+        var title = document.getElementById('title');
+        if (clicked=='false') {
+            title.style.letterSpacing = '32px';
+            var clicked = 'true';
+        }
+        else if (clicked=='true) {
+            title.style.letterSpacing = 'auto';
+            var clicked = 'false';
+        }
+        else { console.log('error') }
     }
