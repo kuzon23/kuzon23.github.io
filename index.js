03d052e857ec22c8d9e93277f74aadf5bb2ddcd8
Matthew <46788940+kuzon23@users.noreply.github.com>
2021-09-20 00:50:09 +1000

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,13 @@
         }
     }
 
+    function updateHTML(elmId, value) {
+        var elem = document.getElementById(elmId);
+        if(typeof elem !== 'undefined' && elem !== null) {
+            elem.innerHTML = value;
+          }
+    }
+
     function poop() {
         var links = ['https://www.youtube.com/watch?v=couz1CrlTdQ','https://www.youtube.com/watch?v=lP0GNfWSW30','https://www.youtube.com/watch?v=2VjpwYM05eo','https://www.youtube.com/watch?v=zfueGqaXp1g', 'https://www.youtube.com/watch?v=SCwcJsBYL3o', 'https://www.youtube.com/watch?v=APJZeNY6dKo', 'https://www.youtube.com/watch?v=IaLGdRx7prA', 'https://www.youtube.com/watch?v=QUqCWkPlLLc'];
         var capybara = links[Math.floor(Math.random()*links.length)];
